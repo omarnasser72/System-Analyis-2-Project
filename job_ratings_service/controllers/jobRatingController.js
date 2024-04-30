f6350c633d1f54adfef2f5944fd7cb1c1b13b011
omarnasser72 <omarnasser7272@gmail.com>
2024-04-30 19:15:35 +0300

    adding validation to jobRatings

--- job_ratings_service/controllers/jobRatingController.js
+++ job_ratings_service/controllers/jobRatingController.js
@@ -1,8 +1,13 @@
+import { validationResult } from "express-validator";
 import JobRating from "../models/JobRating.js";
 import UserJobRating from "../models/userJobRating.js";
 
 export const addJobRating = async (req, res, next) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     console.log("req.body.userId: ", req.body.userId);
     console.log("req.body.jobId: ", req.body.jobId);
 
