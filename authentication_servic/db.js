8d4a62e619e81fb91ea1020f305cb21b424c7262
omarnasser72 <omarnasser7272@gmail.com>
2024-05-01 14:15:38 +0300

    remove ES6

--- authentication_servic/db.js
+++ authentication_servic/db.js
@@ -1,22 +1,17 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 mongoose.set("strictQuery", false);
-const db = process.env.LOCAL_MONGO;
+const db = process.env.MONGO;
 
 const connectDB = async () => {
-    try {
-        console.log(db);
-        await mongoose.connect(db);
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.log(error.message);
-        process.exit(1);
-    }
+  try {
+    console.log(db);
+    await mongoose.connect(db);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 };
 
-
-
-
 module.exports = connectDB;
-
-
--- authentication_servic/index.js
+++ authentication_servic/index.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const connectDB = require('./db');
-const authRoutes = require('./routes/auth');
-const userRoutes = require('./routes/user');
-import cors from "cors";
+const express = require("express");
+const connectDB = require("./db");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 7001;
@@ -10,18 +10,18 @@ const PORT = process.env.PORT || 7001;
 // Connect to MongoDB
 connectDB();
 
-app.use(cors({ origin: "*" }))
+app.use(cors({ origin: "*" }));
 
 // Parse JSON request body
 app.use(express.json());
 
 // Define authentication routes
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
 
 // Define user routes
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
