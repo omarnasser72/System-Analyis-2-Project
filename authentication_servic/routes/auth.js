a6c2b8e0391679db88ba8710de281534ba83143a
omarnasser72 <omarnasser7272@gmail.com>
2024-05-01 16:30:20 +0300

    fix database connection in auth service

--- authentication_servic/routes/auth.js
+++ authentication_servic/routes/auth.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const { register, login } = require('../controllers/auth');
+const express = require("express");
+const { register, login, healthCheck } = require("../controllers/auth");
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post("/register", register);
+router.post("/login", login);
+
+//health check
+router.get("/healthCheck", healthCheck);
 
 module.exports = router;
