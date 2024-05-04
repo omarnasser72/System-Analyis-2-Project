0a6b8f28c7d3c51b528ef56ba16021fef78660fa
Your Name <you@example.com>
2024-05-04 20:30:34 +0300

    edite

--- companies_service/routes/company-router.js
+++ companies_service/routes/company-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const CampanyController = require("../controllers/campany_controller");
 const router = express.Router();
 
-router.get("/campanies", CampanyController.getCampanies);
+router.get("/companies", CampanyController.getCampanies);
 router.get("/", CampanyController.checkServiceRunning);
 
 module.exports = router;
